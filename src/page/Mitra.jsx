acaf04f562b02b9c9c2640e477f5489b33d558dc
rifkitaufikurrohman <pakiwiki123@gmail.com>
2024-11-12 15:32:27 +0700

    responsive mitra page

--- src/page/Mitra.jsx
+++ src/page/Mitra.jsx
@@ -1,27 +1,56 @@
 import React from "react";
 import img1 from "../assets/image/img_mitra_1.png";
+import Wrapper from "../components/Wrapper";
 
 const Mitra = () => {
   return (
     <>
-      <section id="mitra">
-        <div className="mt-10">
-          <div className="mx-14 py-12 px-9 custom-shadow rounded-2xl">
-            <div className="flex items-center">
-              <div className="w-[55%] flex flex-col gap-y-10">
-                <h1 className="text-5xl font-bold">Durian Delights</h1>
-                <p className="text-2xl font-normal max-w-2xl">
-                  Menyediakan berbagai jenis durian premium dari berbagai daerah. Mengutamakan kualitas rasa dengan pilihan durian segar dan produk olahan durian, seperti es krim, pancake, dan smoothie.
-                </p>
-                <button className="w-1/2 p-2 bg-yellow-400 text-stone-900 font-semibold rounded-[10px] transition duration-[400ms] ease-in-out hover:bg-yellow-600">Pelajari lebih lanjut</button>
-              </div>
-              <div className="flex justify-center w-[45%]">
-                <img src={img1} className="w-full " alt="Mitra 1" />
+      <Wrapper>
+        <section id="mitra">
+          <div className="mt-10">
+            <div className="p-6 custom-shadow rounded-2xl lg:p-12">
+              <div className="flex flex-col justify-center items-center gap-y-4 lg:flex-row gap-x-10">
+                <div className="flex justify-center w-full sm:w-[90%] md:w-[80%] lg:order-2 lg:w-1/2">
+                  <img src={img1} className="w-full " alt="Mitra_1" />
+                </div>
+                <div className="flex flex-col gap-y-4 lg:order-1 lg:w-1/2 lg:gap-y-8">
+                  <h1 className="text-2xl font-bold md:text-center lg:text-5xl lg:text-left">Durian Delights</h1>
+                  <p className="text-xl text-justify lg:text-2xl lg:text-left">
+                    Menyediakan berbagai jenis durian premium dari berbagai daerah. Mengutamakan kualitas rasa dengan pilihan durian segar dan produk olahan durian, seperti es krim, pancake, dan smoothie.
+                  </p>
+                  <div className="flex justify-center lg:justify-start">
+                    <button className="w-full p-2 bg-yellow-400 text-stone-900 font-semibold rounded-[10px] transition duration-[400ms] ease-in-out hover:bg-yellow-600 sm:w-[40%] lg:w-1/2 xl:text-xl">
+                      Pelajari lebih lanjut
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Wrapper>
+
+
+      {/* <Wrapper >
+        <section id="mitra">
+          <div className="mt-10">
+            <div className="py-12 px-9 custom-shadow rounded-2xl">
+              <div className="flex items-center">
+                <div className="w-[55%] flex flex-col gap-y-10">
+                  <h1 className="text-5xl font-bold">Durian Delights</h1>
+                  <p className="text-2xl font-normal max-w-2xl">
+                    Menyediakan berbagai jenis durian premium dari berbagai daerah. Mengutamakan kualitas rasa dengan pilihan durian segar dan produk olahan durian, seperti es krim, pancake, dan smoothie.
+                  </p>
+                  <button className="w-1/2 p-2 bg-yellow-400 text-stone-900 font-semibold rounded-[10px] transition duration-[400ms] ease-in-out hover:bg-yellow-600">Pelajari lebih lanjut</button>
+                </div>
+                <div className="flex justify-center w-[45%]">
+                  <img src={img1} className="w-full " alt="Mitra 1" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Wrapper> */}
     </>
   );
 };
