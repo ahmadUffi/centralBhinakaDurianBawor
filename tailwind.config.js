2f4829a626363813d7df3141a8b6138c6abe3df0
ahmadUffi <ahmaduffi45@gmail.com>
2024-11-14 22:26:22 +0700

    marge uffi rifki

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-<<<<<<< HEAD
       backgroundImage: {
         BgWelcoming: "url('./src/assets/image/background.png')",
         BgCatalogDuren: "url('./src/assets/image/bgCatalogDuren.jpg')",
       },
       colors: {
-        primary: "#FCC52D",
+        primary: "#343131",
+        lightYellow: "#FDE050",
+        darkYellow: "#FCC52D",
       },
-=======
-      colors: {
-        'primary': "#343131",
-        'light-yellow' : "#FDE050",
-        'dark-yellow' : "#FCC52D"
-      }
->>>>>>> 9c4ea1bd0f22a5b8765f63b4330fb33659152815
     },
   },
   plugins: [],
