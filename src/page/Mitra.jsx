8e15d4958ffac0b4719ff743f783ffd5321a16fb
Ramadhan <whyramadhan27@gmail.com>
2024-11-17 20:28:06 +0700

    remove unused component file

--- src/page/Mitra.jsx
+++ src/page/Mitra.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Wrapper from "../components/Wrapper";
 
 const Mitra = () => {
-  return <div>Mitra</div>;
+  return (
+    <Wrapper>
+      <div className={`py-24 justify-center items-center`}>
+        <header>
+          <h1>SELAMAT DATANG DI MITRA BAWOR</h1>
+        </header>
+      </div>
+
+      {/* <div>
+        <h1>Hello World</h1>
+      </div> */}
+    </Wrapper>
+  );
 };
 
 export default Mitra;
