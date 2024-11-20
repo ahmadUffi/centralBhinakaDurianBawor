3fe57de589fd8c150d449369adc6bc508a28f648
ahmadUffi <ahmaduffi45@gmail.com>
2024-11-20 09:01:38 +0700

    fix catalog

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -3,30 +3,23 @@ import logo from "../assets/image/logo2.png";
 
 const Catalog = () => {
   return (
-    <div className="catalog">
-      <div className="relative content w-[600px] h-[300px] bg-BgCatalogDuren bg-cover bg-center p-4 overflow-hidden">
+    <div className="relative catalog p-2 rounded-lg bg-lightYellow md:min-w-[600px] md:h-[300px]  min-w-[350px] h-[200px] inline-block ">
+      <div className="relative content w-full h-full bg-BgCatalogDuren bg-cover bg-center py-4 px-4 rounded-lg">
         <div className="logo absolute top-0 left-0">
           <img src={logo} alt="logo" width={"40%"} />
         </div>
-        <div className="content_catalog absolute bottom-0 left-auto">
+        <div className="content_catalog absolute bottom-0 text-white py-4 px-4">
           <div className="title w-max ">
-            <h6 className="font-medium text-white py-1 px-2 rounded-md bg-[#4967FF]">
+            <h6 className="font-medium text-white py-1 px-2 rounded-md bg-[#4967FF] text-[13px] md:text-[20px] mb-2">
               Buah Durian
             </h6>
           </div>
-          <p>
+          <p className="font-medium text-[13px] md:text-[20px] text-justify leading-4 md:leading-7">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem
             enim non in aliquid, optio unde earum porro dolores sapiente nemo!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea maiores
-            fugit beatae accusamus eaque incidunt temporibus sapiente aperiam
-            laudantium qui!
           </p>
         </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   );
 };
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,18 +5,36 @@ import Welcome from "../components/Welcome";
 import Catalog from "../components/Catalog";
 
 function Home() {
+  const demo = [1, 2, 3];
   return (
     <>
       <Welcome />
-      <div className="fild">
-        <div className="title">
-          <h1 className="text-[38px] text-center font-medium">
-            Ini akan menjadi alasan kelezatan, dengan <br /> cita rasa unik dan
-            menarik
-          </h1>
-          <Catalog />
+      <Wrapper>
+        <div className="fild">
+          {/* Catalogs */}
+          <div className="catalog-container">
+            <div className="title-catalog">
+              <h1 className="md:text-[38px] text-[24px] text-center font-medium mt-9">
+                Ini akan menjadi alasan kelezatan, dengan{" "}
+                <br className="hidden md:block" /> cita rasa unik dan menarik
+              </h1>
+            </div>
+            <div className="catalogs flex overflow-x-auto gap-[20px] mt-5 ">
+              {demo.map((catalog, index) => (
+                <Catalog key={index} />
+              ))}
+            </div>
+          </div>
+          {/* akhir catalog */}
+          {/* Komunitas */}
+          <div className="komunitas-container">
+            <h1 className="md:text-[38px] text-[24px] text-center mt-9 uppercase font-bold ">
+              Komunitas durian bawor
+            </h1>
+          </div>
+          {/* akhir komunitas */}
         </div>
-      </div>
+      </Wrapper>
     </>
   );
 }
