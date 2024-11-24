9ad6b655be283c91f548b09116266d9a63705e56
Ramadhan <whyramadhan27@gmail.com>
2024-11-24 15:17:20 +0700

    add carousel component and update dependencies; integrate react-slick for product display

--- src/page/MitraPage.jsx
+++ src/page/MitraPage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import backgroundImage from "../assets/image/background.png";
+import durian from "../assets/image/durian.svg";
+import ScrollableCard from "../components/CatalogCarousel";
+import image1 from "../assets/image/img_mitra_1.png";
+import image2 from "../assets/image/img_mitra_1.png";
+
+const cardsData = [
+  { image: image1, description: "Durian Delights" },
+  { image: image2, description: "Durian Delights" },
+  { image: image1, description: "Durian Delights" },
+];
 
 const MitraPage = () => {
   return (
@@ -17,23 +27,38 @@ const MitraPage = () => {
           Unik dan Menarik
         </p>
       </div>
-      <div className="w-[968px] h-[447px] bg-darkYellow flex justify-center py-1">
-        <div
-          className="w-[773px] h-[436px] flex justify-center items-center"
-          style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-          }}
-        >
-          <div className="w-full h-full">
-            <h1 className="font-bold">DURIAN BAWOR BANYUMAS</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eos
-              labore fugit vero tenetur pariatur iusto officia dicta quisquam
-              minus.
+      <div className="flex items-center justify-center w-[968px] h-[447px] bg-darkYellow">
+        <div className="bg-white rounded-lg shadow-lg p-8 flex flex-col md:flex-row items-center w-[773px] h-[436px]">
+          <div className="flex-1 text-center md:text-left">
+            <h1 className="text-3xl font-bold mb-4 text-center">
+              DURIAN BAWOR BANYUMAS
+            </h1>
+            <p className="text-gray-700 text-center">
+              Kami adalah komunitas yang berfokus pada keberagaman dan
+              pelestarian lingkungan, dengan semangat menciptakan perubahan
+              positif bagi lingkungan dan masyarakat sekitar.
             </p>
           </div>
+          <div className="flex-1 mt-6 md:mt-0">
+            <img
+              src={durian}
+              alt="Durian"
+              className="w-full max-w-xs mx-auto"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-10 justify-between items-center text-center">
+        <h1 className="font-semibold text-xl max-w-xl ">
+          Lebih dari 1.000 orang telah merasakan kenikmatannya, menjadikan
+          favorit baru yang menggugah selera.
+        </h1>
+
+        <p>catalog Durian Bawor</p>
+
+        {/* Banner */}
+        <div className="w-full px-4 py-8">
+          <ScrollableCard cards={cardsData} />
         </div>
       </div>
     </div>
