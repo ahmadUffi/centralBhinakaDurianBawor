7b16c5f83a79277d453b1c6ffc2f5703ac8adb7e
rifkitaufikurrohman <pakiwiki123@gmail.com>
2024-12-06 12:52:51 +0700

    edit product

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import star from "../assets/image/ic-star.svg"
-import { wa } from "../data"
+import React from "react";
+import star from "../assets/image/ic-star.svg";
+import { wa } from "../data";
 
 function CardProduct({ titleContent, titleCard, desc, image }) {
   const numStars = 5;
@@ -9,20 +9,20 @@ function CardProduct({ titleContent, titleCard, desc, image }) {
   let styleImage;
 
   switch (titleContent) {
-    case "productBibit":
-        styleBgGradient = "linear-gradient(150deg, #A7BC2E 45%, #FFFFFF 45%)";
-        styleButton = "py-2 px-3 rounded-lg bg-[#A7BC2E]";
-        styleImage = "custom-box-shadow-green size-[120px] bg-[#F9F9F6FF] rounded-full sm:size-[130px]";
+    case "productsBibit":
+      styleBgGradient = "linear-gradient(150deg, #A7BC2E 45%, #FFFFFF 45%)";
+      styleButton = "py-2 px-3 rounded-lg bg-[#A7BC2E]";
+      styleImage = "custom-box-shadow-green size-[120px] bg-[#F9F9F6FF] rounded-full sm:size-[130px]";
       break;
-    case "productDurian":
-        styleBgGradient = "linear-gradient(150deg, #FCC52D 45%, #FFFFFF 45%)";
-        styleButton = "py-2 px-3 rounded-lg bg-darkYellow";
-        styleImage = "custom-box-shadow size-[120px] bg-[#F9F9F6FF] rounded-full";
+    case "productsDurian":
+      styleBgGradient = "linear-gradient(150deg, #FCC52D 45%, #FFFFFF 45%)";
+      styleButton = "py-2 px-3 rounded-lg bg-darkYellow";
+      styleImage = "custom-box-shadow size-[120px] bg-[#F9F9F6FF] rounded-full";
       break;
-    case "productOlahan":
-        styleBgGradient = "linear-gradient(150deg, #FFE700 45%, #ffffff 45%)";
-        styleButton = "py-2 px-3 rounded-lg bg-[#FFE700]";
-        styleImage = "custom-box-shadow-lightYellow size-[120px] bg-[#F9F9F6FF] rounded-full"
+    case "productsOlahan":
+      styleBgGradient = "linear-gradient(150deg, #FFE700 45%, #ffffff 45%)";
+      styleButton = "py-2 px-3 rounded-lg bg-[#FFE700]";
+      styleImage = "custom-box-shadow-lightYellow size-[120px] bg-[#F9F9F6FF] rounded-full";
       break;
     default:
       break;
@@ -30,20 +30,13 @@ function CardProduct({ titleContent, titleCard, desc, image }) {
   console.log(titleContent);
   return (
     <>
-    { console.log(titleContent) }
-      <div 
-        className="card-product flex flex-col gap-y-8 p-4 custom-shadow bg-gradient-to-r from-darkYellow to-white rounded-xl sm:w-[44%] lg:w-[28%]" style={{ background: styleBgGradient }}
-      >
+      <div className="card-product flex flex-col gap-y-8 p-4  custom-shadow bg-gradient-to-r from-darkYellow to-white rounded-xl sm:w-[44%] lg:w-[28%]" style={{ background: styleBgGradient }}>
         <div className="flex flex-col gap-y-2">
-          <h2 className="text-xl font-semibold">
-            { titleCard }
-          </h2>
-          <p className="text-md max-w-[300px]">
-            { desc }
-          </p>
+          <h2 className="text-xl font-semibold">{titleCard}</h2>
+          <p className="text-md max-w-[300px]">{desc}</p>
         </div>
         <div className="flex justify-center">
-          <img src={ image } className={`${ styleImage }`} alt="img-card-product" />
+          <img src={image} className={`${styleImage}`} alt="img-card-product" />
         </div>
         <div className="flex justify-between items-center">
           <div className="flex gap-x-1">
@@ -51,8 +44,10 @@ function CardProduct({ titleContent, titleCard, desc, image }) {
               <img src={star} key={index} className="size-4" alt="ic-star" />
             ))}
           </div>
-          <div className={ styleButton }>
-            <a href={`${ wa }6289603014813`} className='text-primary font-semibold' target="_blank">Hubungi</a>
+          <div className={styleButton}>
+            <a href={`${wa}6289603014813`} className="text-primary font-semibold" target="_blank">
+              Hubungi
+            </a>
           </div>
         </div>
       </div>
